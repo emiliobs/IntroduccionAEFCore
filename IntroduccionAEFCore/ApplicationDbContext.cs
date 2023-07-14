c5c4538cc8d043a849dd8402fb39ebe7e7f995f8
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-07-13 19:22:56 -0500

    cclase dataContex, Genero, migracion y creacion de labd con EFC

---
+++ IntroduccionAEFCore/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IntroduccionAEFCore.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroduccionAEFCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Genero> Generos => Set<Genero>();
+    }
+}
---
+++ IntroduccionAEFCore/Entidades/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace IntroduccionAEFCore.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+
+        public String Nombre { get; set; } = null!;
+    }
+}
