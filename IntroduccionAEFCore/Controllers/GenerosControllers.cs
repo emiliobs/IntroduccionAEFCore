dddf9d3559eb7646184c4503dfeae53d3ba272f4
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-07-24 12:23:15 -0500

    INserter dato por medio de post en la API  en genero identity

---
+++ IntroduccionAEFCore/Controllers/GenerosControllers.cs
@@ -0,0 +1,27 @@
+﻿using IntroduccionAEFCore.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroduccionAEFCore.Controllers
+{
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosControllers : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenerosControllers(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Genero genero)
+        {
+
+            _context.Add(genero);
+            await _context.SaveChangesAsync();
+
+            return Ok(genero);
+        }
+    }
+}
